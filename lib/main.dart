790f4b6b2d3b893ab690b090d3df55cce615a1b5
barbara.rodrigues <barbara.rodrigues@neocred.online>
2021-07-08 09:11:29 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+         // child: RiveAnimation.asset('assets/animations/animated_heart.riv')
+        ),
+      ),
+    );
+  }
+}
